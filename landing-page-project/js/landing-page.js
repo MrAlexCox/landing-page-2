dcfa50f5c1a160b119588691f06acd61a8997584
MrAlexCox <afcox90@gmail.com>
2021-02-02 14:04:23 +0000

    Latest updates

---
+++ landing-page-project/js/landing-page.js
@@ -0,0 +1,25 @@
+let i = 0;
+let txt = 'This is the title';
+let speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("title").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+document.getElementById("title").onload = function() {typeWriter()};
+
+
+
+// function jump(h){
+//     var top = document.getElementById(h).offsetTop;
+//     window.scrollTo(0, top);
+// }
+
+function scroll(h) {
+  let element = document.getElementById('h');
+  element.scrollIntoView({behaviour: 'smooth', block: 'offsetTop', inline: "nearest"})
+}
