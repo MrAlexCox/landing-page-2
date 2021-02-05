480bd5691cad9535286ab384514377bce23fb207
MrAlexCox <afcox90@gmail.com>
2021-02-05 10:25:23 +0000

    Final version

--- landing-page-project/js/landing-page.js
+++ landing-page-project/js/landing-page.js
@@ -1,25 +1,43 @@
-let i = 0;
-let txt = 'This is the title';
-let speed = 50;
+// step 1: for all elements with the name segment, loop over a for loop and append section + [i] to the nav background
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("title").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+let sections = document.getElementsByClassName('segment');
+let i = 0;
+let nav = document.getElementById('nav-list')
+
+for (i=1; i < sections.length; i++) {
+    let b = 'Section ' + [i];
+    let c = 'section-' + [i];
+    nav.innerHTML += '<div class=\'section-select\' onclick="buttonScroll(\'' + c + '\');">' + b + '</div>'
+}
+
+
+
+function jump(h){
+    var top = document.getElementById(h).offsetTop;
+    window.scrollTo(0, top);
+}
+
+function buttonScroll(h) {
+  let element = document.getElementById(h);
+  element.scrollIntoView({behaviour: 'smooth', inline: "nearest"})
+}
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
 }
 
-document.getElementById("title").onload = function() {typeWriter()};
-
-
-
-// function jump(h){
-//     var top = document.getElementById(h).offsetTop;
-//     window.scrollTo(0, top);
-// }
-
-function buttonScroll(h) {
-  let element = document.getElementById('h');
-  element.scrollIntoView({behaviour: 'smooth', block: 'offsetTop', inline: "nearest"})
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
